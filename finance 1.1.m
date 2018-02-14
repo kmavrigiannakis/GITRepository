b02f88a411e0d0a6a587a1215df9b4df712254ea
kmavrigiannakis <36475272+kmavrigiannakis@users.noreply.github.com>
2018-02-14 17:02:15 +0200

    Initial Code

--- finance 1.1.m
+++ finance 1.1.m
@@ -1,9 +1,3 @@
-%xmin= -1;
-%xmax =1;
-%rho = xmin +rand(1)* (xmax-xmin);  %creation of a univariate uniform random variable, and normalize it at range -1 - 1
-%while (abs(rho)<0.3)
-%    rho = xmin +rand(1,1)* (xmax-xmin);
-%end
 clear;
 clc;
 
@@ -27,18 +21,4 @@ figure
 plot(Y, normpdf(Y,0,1));
 
 reg1 = regstats(Y,X)
-
-
-
-% subplot(2,1,1) 
-% plot(X, normpdf(X,0,1));
-% title('X')
-% 
-% subplot(2,1,2) 
-% plot(Y, normpdf(Y,0,1));
-% title('Y')
-% 
-% subplot(2,1,3) 
-% plot(Z, normpdf(Z,0,1));
-% title('Z')
 
