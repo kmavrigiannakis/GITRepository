625e9a43f223a851823a2b7f36122c3398065cb8
kmavrigiannakis <36475272+kmavrigiannakis@users.noreply.github.com>
2018-02-15 02:07:55 +0200

    Update finance 1.1.m

--- finance 1.1.m
+++ finance 1.1.m
@@ -20,6 +20,22 @@ corrcoef(Y,X)
 figure
 plot(Y, normpdf(Y,0,1));
 
-reg1 = regstats(Y,X)
+reg1 = regstats(Y,X, 'linear');
 
-%regression
+
+beta = reg1.tstat.beta;
+CoefficientX = beta(2,1);
+Constant = beta(1,1);
+
+
+t = reg1.tstat.t;
+
+Pvalues=reg1.tstat.pval;
+
+Rsquare = reg1.rsquare;
+RMSE = sqrt(reg1.mse);
+
+
+disp(reg1.tstat.se)
+
+save( 'Results.mat', 'X','Y' );
